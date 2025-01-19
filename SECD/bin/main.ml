25c7d5302c9a733758b042f0d08a714bffbc3045
Eric Lee <me@ericchanlee.com>
2025-01-19 15:53:53 -0800

    [Main] REPL implementation

--- SECD/bin/main.ml
+++ SECD/bin/main.ml
@@ -1,1 +1,32 @@
 open Lib
+
+let unwrap (err : string) (opt : 'a option)  : 'a =
+  match opt with
+  | None -> failwith err
+  | Some x -> x
+
+let execute (p : string) : string =
+  p
+  |> Intro.parse
+  |> unwrap "failed to parse"
+  |> Flatten.flatten
+  |> SECD.init
+  |> SECD.eval
+  |> SECD.string_of_value
+
+let rec repl () =
+  print_string ">>> ";
+  let input = read_line () in
+  if input = ":q" then
+    print_endline "Goodbye!"
+  else
+    try
+      print_endline (execute input);
+      repl ()
+    with e ->
+      Printf.printf "Error: %s\n" (Printexc.to_string e);
+      repl ()
+
+let () =
+  print_endline "Welcome to the SECD REPL (type ':q' to quit)";
+  repl ()
