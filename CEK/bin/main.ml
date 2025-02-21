2fc84d144f722271bc6fd41785ad6278b83212d5
Eric Lee <me@ericchanlee.com>
2025-02-20 16:48:55 -0800

    CLI Options to run from file or string without repl

--- CEK/bin/main.ml
+++ CEK/bin/main.ml
@@ -1,62 +1,23 @@
-open Lib
-
-let multiline = ref false
-
-(** Executes [f] but prints [err] on error, for REPL purposes *)
-let ignore_err f =
-    try
-      f ()
-    with e ->
-      Printf.printf "Error: %s\n" (Printexc.to_string e)
-
-(** Gets line for stdin, waits for double newlines if [multiline] is on *)
-let rec get_input () =
-  let line = read_line () in
-  if !multiline && line <> "" && String.get line 0 <> ':' then
-    line ^ "\n" ^ get_input ()
-  else
-    line
-
-(** Executes a CEK [program] and prints out the result *)
-let print_execute program =
-  program
-  |> Compile.execute
-  |> CEK.string_of_value
-  |> print_endline
-
-(** CEK REPL *)
-let rec repl () =
-  print_string ">>> ";
-  let input = get_input () in
-  begin
-    match input with
-    | c when List.mem c [":q"; ":quit"] ->
-        print_endline "Goodbye!";
-        exit 0
-    | c when List.mem c [":h"; ":help"] ->
-        print_endline @@
-          ":h(elp) -> View this message\n" ^
-          ":q(uit) -> Exit REPL\n" ^
-          ":m(ulti) -> Toggle multiline mode, uses double newlines to run\n" ^
-          ":r(un) <file> -> Executes contents of <file> as a string";
-    | c when List.mem c [":m"; ":multi"] ->
-        multiline := not !multiline;
-        print_endline @@
-          "Set multiline mode to " ^ string_of_bool !multiline;
-    | c when String.starts_with ~prefix:":r" c
-          || String.starts_with ~prefix:":run" c ->
-        ignore_err (fun () ->
-          match String.split_on_char ' ' input with
-          | [_; fname] ->
-              print_execute (In_channel.with_open_text fname In_channel.input_all)
-          | _ -> failwith "Unexpected :r syntax, See :h(elp)");
-    | c when String.starts_with ~prefix:":" c ->
-        print_endline "Error: Unexpected meta command, See :h(elp)";
-    | _ ->
-        ignore_err (fun () -> print_execute input);
-  end;
-  repl ()
+let help = "Usage: dune exec CEK -- [-file FILENAME] [-repl]"
 
 let () =
-  print_endline "Welcome to the CEK REPL (type ':h(elp)' for meta commands)";
-  repl ()
+  let repl = ref false in
+  let filename = ref "" in
+  let code = ref "" in
+  let speclist =
+    [
+      ("-repl", Arg.Set repl, "REPL Mode");
+      ("-file", Arg.Set_string filename, "File to Execute");
+      ("-code", Arg.Set_string code, "String to Execute");
+    ]
+  in
+
+  Arg.parse speclist (Fun.const ()) help;
+
+  match (!repl, !filename, !code) with
+  | true, _, _ -> Repl.start_repl ()
+  | _, "", "" -> Arg.usage speclist help
+  | _, "", code -> Repl.print_execute code
+  | _, path, _ ->
+      let open In_channel in
+      Repl.print_execute (input_all (open_text path))
