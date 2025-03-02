9963ac5d88369cea4d6118d8256b616dfb5adc8e
Eric Lee <me@ericchanlee.com>
2025-03-02 10:32:52 -0800

    Krivine Machine frontend (no recursion)

--- Krivine/lib/parse.ml
+++ Krivine/lib/parse.ml
@@ -7,9 +7,14 @@ let rec parse (e : Intro.t) : Krivine.t =
   | Int n -> Int n
   | Var v -> Var v
   | If (c, t, f) -> If (parse c, parse t, parse f)
-  | Lambda (args, body) -> Lambda (List.nth args 0, parse body)  (* TODO *)
+
+  | Lambda (args, body) -> List.fold_right (fun arg acc -> Lambda (arg, acc)) args (parse body)
+  (* | Lambda (args, body) -> Lambda (List.nth args 0, parse body)  (* TODO *) *)
+
   | LambdaRec _ -> failwith "TODO"
-  | Call (f, es) -> App (parse f, parse (List.nth es 0))  (* TODO *)
+
+  | Call (f, es) -> List.fold_left (fun acc arg -> App (acc, parse arg)) (parse f) es
+  (* | Call (f, es) -> App (parse f, parse (List.nth es 0))  (* TODO *) *)
 
   | Prim Add -> Add
   | Prim Sub -> Sub
